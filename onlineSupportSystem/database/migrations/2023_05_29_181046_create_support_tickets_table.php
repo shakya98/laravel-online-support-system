de2ae1105e76ff47f68333b02a6ab10d017f753d
shakya <shakya980528@gmail.com>
2023-05-29 11:20:14 -0700

    Creating support ticket model with migration file

---
+++ onlineSupportSystem/database/migrations/2023_05_29_181046_create_support_tickets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('support_tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_name');
+            $table->text('problem_description');
+            $table->string('email');
+            $table->string('phone_number');
+            $table->string('reference_number')->unique();
+            $table->boolean('is_open')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('support_tickets');
+    }
+};
